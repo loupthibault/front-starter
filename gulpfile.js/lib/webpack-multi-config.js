75b7ef5f5d3a770c3cbe0db30ae7c268a1bb1712
Loup-Rodolphe THIBAULT <loupthibault@gmail.com>
2017-03-24 18:28:14 +0100

    switch back to  nunjucks 1.14.0 because of loaders still not ready for webpack2

--- gulpfile.js/lib/webpack-multi-config.js
+++ gulpfile.js/lib/webpack-multi-config.js
@@ -10,125 +10,147 @@ module.exports = function(env) {
   const jsDest  = path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest);
   const publicPath = pathToUrl(TASK_CONFIG.javascripts.publicPath || PATH_CONFIG.javascripts.dest, '/');
 
-  const extensions = TASK_CONFIG.javascripts.extensions.map( (extension) => '.' + extension );
+  const dotExtensions = TASK_CONFIG.javascripts.extensions.map( (extension) => '.' + extension );
 
   const filenamePattern = '[name].js';
 
-  const defaultBabelConfig = {
-    presets: ['es2015']
+  // Attach default babel loader config to webpack
+  let babelLoader = {
+    test: new RegExp(`(\\${dotExtensions.join('$|\\.')}$)`),
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+    query: TASK_CONFIG.javascripts.babel || {
+      presets: ['es2015']
+    }
   };
+
+  // if custom babel loader config is present extend the given configuration
+  if (TASK_CONFIG.javascripts.babelLoader !== undefined) {
+    babelLoader = _object.assign(babelLoader, TASK_CONFIG.javascripts.babelLoader);
+  }
 
   const testPattern = new RegExp(`(\\${TASK_CONFIG.javascripts.extensions.join('$|\\.')}$)`);
 
   const webpackConfig = {
     context: jsSrc,
     output: {},
-    plugins: [],
+    plugins: [
+      new webpack.optimize.OccurenceOrderPlugin()
+    ],
     resolve: {
-      modules: [
-        jsSrc,
-        path.resolve(process.env.PWD, 'node_modules')
-      ],
-      extensions: [].concat(extensions),
-      alias: TASK_CONFIG.javascripts.alias
-    },
+      root: jsSrc,
+      extensions: [''].concat(dotExtensions),
+      alias: TASK_CONFIG.javascripts.alias,
+      fallback: path.resolve(process.env.PWD, 'node_modules')
+    }, // See https://github.com/facebook/react/issues/4566
     resolveLoader: {
-      modules: [ path.resolve(process.env.PWD, 'node_modules') ]
+      fallback: path.resolve(process.env.PWD, 'node_modules')
     },
     module: {
-
-      rules: [
-        {
-          test: testPattern,
-          exclude: [/node_modules/],
-          use: [{
-            loader: 'babel-loader',
-            options: TASK_CONFIG.javascripts.babel || defaultBabelConfig
-          }],
-        }
-      ]
+      loaders: [babelLoader]
     }
   };
 
-  // Add additional loaders from config
-  webpackConfig.module.rules = webpackConfig.module.rules.concat(TASK_CONFIG.javascripts.rules || []);
-
-  if(env === 'development') {
-
-    webpackConfig.devtool         = TASK_CONFIG.javascripts.devtool || 'eval-cheap-module-source-map';
-    webpackConfig.output.pathinfo = true;
-
-    // Create new entries object with webpack-hot-middleware and react-hot-loader (if enabled)
-    if(!TASK_CONFIG.javascripts.hot || TASK_CONFIG.javascripts.hot.enabled !== false) {
-      for (var key in TASK_CONFIG.javascripts.entries) {
-        var entry = TASK_CONFIG.javascripts.entries[key];
-        // TODO: To work in < node 6, prepend process.env.PWD + node_modules/
-        const entries = [];
-        let middleware = 'webpack-hot-middleware/client?';
-
-        if(!TASK_CONFIG.javascripts.hot || TASK_CONFIG.javascripts.hot.reload !== false) {
-          middleware+= '&reload=true';
-        }
-
-        if(TASK_CONFIG.javascripts.hot && TASK_CONFIG.javascripts.hot.noInfo) {
-          middleware+= '&noInfo=true';
-        }
-
-        if(TASK_CONFIG.javascripts.hot && TASK_CONFIG.javascripts.hot.quiet) {
-          middleware+= '&quiet=true';
-        }
-
-        if(TASK_CONFIG.javascripts.hot && TASK_CONFIG.javascripts.hot.react) {
-          entries.push('react-hot-loader/patch');
-        }
-
-        TASK_CONFIG.javascripts.entries[key] = entries.concat(middleware).concat(entry);
-      }
-
-      webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-    }
-    // Addtional loaders for dev
-    webpackConfig.module.rules = webpackConfig.module.rules.concat(TASK_CONFIG.javascripts.developmentRules || []);
+  
+  // Provide global objects to imported modules to resolve dependencies (e.g. jquery)
+  if (TASK_CONFIG.javascripts.provide) {
+    webpackConfig.plugins.push(new webpack.ProvidePlugin(TASK_CONFIG.javascripts.provide))
   }
 
-  if(env !== 'test') {
+  if (env === 'development') {
+    webpackConfig.devtool = TASK_CONFIG.javascripts.devtool || 'eval-cheap-module-source-map'
+    webpackConfig.output.pathinfo = true
+
+    // Create new entries object with webpack-hot-middleware and react-hot-loader (if enabled)
+    if (!TASK_CONFIG.javascripts.hot || TASK_CONFIG.javascripts.hot.enabled !== false) {
+      for (var key in TASK_CONFIG.javascripts.entries) {
+        var entry = TASK_CONFIG.javascripts.entries[key]
+        // TODO: To work in < node 6, prepend process.env.PWD + node_modules/
+        const entries = []
+        let middleware = 'webpack-hot-middleware/client?'
+
+        if (!TASK_CONFIG.javascripts.hot || TASK_CONFIG.javascripts.hot.reload !== false) {
+          middleware += '&reload=true'
+        }
+
+        if (TASK_CONFIG.javascripts.hot && TASK_CONFIG.javascripts.hot.noInfo) {
+          middleware += '&noInfo=true'
+        }
+
+        if (TASK_CONFIG.javascripts.hot && TASK_CONFIG.javascripts.hot.quiet) {
+          middleware += '&quiet=true'
+        }
+
+        if (TASK_CONFIG.javascripts.hot && TASK_CONFIG.javascripts.hot.react) {
+          entries.push('react-hot-loader/patch')
+        }
+
+        TASK_CONFIG.javascripts.entries[key] = entries.concat(middleware).concat(entry)
+      }
+
+      webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
+    }
+  }
+
+  if (env !== 'test') {
     // Karma doesn't need entry points or output settings
-    webpackConfig.entry = TASK_CONFIG.javascripts.entries;
+    webpackConfig.entry = TASK_CONFIG.javascripts.entries
 
-    webpackConfig.output.path = path.normalize(jsDest);
-    webpackConfig.output.filename = filenamePattern;
-    webpackConfig.output.publicPath = publicPath;
+    webpackConfig.output.path = path.normalize(jsDest),
+      webpackConfig.output.filename = filenamePattern,
+      webpackConfig.output.publicPath = publicPath
 
-    if(TASK_CONFIG.javascripts.extractSharedJs) {
+    if (TASK_CONFIG.javascripts.extractSharedJs) {
       // Factor out common dependencies into a shared.js
       webpackConfig.plugins.push(
         new webpack.optimize.CommonsChunkPlugin({
           name: 'shared',
           filename: filenamePattern,
         })
-      );
+      )
     }
-
-    // Addtional loaders for tests
-    webpackConfig.module.rules = webpackConfig.module.rules.concat(TASK_CONFIG.javascripts.testRules || []);
   }
 
-  if(env === 'production') {
-    
+  if (env === 'production') {
+    if (rev) {
+      webpackConfig.plugins.push(new webpackManifest(PATH_CONFIG.javascripts.dest, PATH_CONFIG.dest))
+    }
+
     webpackConfig.plugins.push(
       new webpack.DefinePlugin({
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
         }
       }),
+      new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin(),
       new webpack.NoErrorsPlugin()
     );
-
-    // Addtional loaders for production
-    webpackConfig.module.rules = webpackConfig.module.rules.concat(TASK_CONFIG.javascripts.productionRules || [])
   }
 
-  return webpackConfig;
 
+  // Add defined plugins and loaders for all environments
+  if( TASK_CONFIG.javascripts.plugins ) {
+    webpackConfig.plugins = webpackConfig.plugins.concat(TASK_CONFIG.javascripts.plugins(webpack) || [])
+  }
+  webpackConfig.module.loaders = webpackConfig.module.loaders.concat(TASK_CONFIG.javascripts.loaders || [])
+
+  /**
+   * Additional loaders for development and production
+   *
+   * @deprecated since version 4.0.0, define additional loaders in javascripts.development.loaders
+   */
+  if (TASK_CONFIG.javascripts[env+'Loaders']) {
+    webpackConfig.module.loaders = webpackConfig.module.loaders.concat(TASK_CONFIG.javascripts[env+'Loaders'] || [])
+  }
+
+  // Additional plugins and loaders according to environment
+  if (TASK_CONFIG.javascripts[env]) {
+    if( TASK_CONFIG.javascripts[env].plugins ) {
+      webpackConfig.plugins = webpackConfig.plugins.concat(TASK_CONFIG.javascripts[env].plugins(webpack) || [])
+    }
+    webpackConfig.module.loaders = webpackConfig.module.loaders.concat(TASK_CONFIG.javascripts[env].loaders || [])
+  }
+
+  return webpackConfig
 };
